86c52cde0f2405cd9da74832f6a8fe154b3fd562
Alex Mumina <alexmumissh@gmail.com>
2026-05-25 23:31:20 +0300

    fixed bugs, responsive design, high/low hints

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const inputEl = document.getElementById("inputValue")
 const secretEl = document.getElementById("readOnly")
 const messageEl = document.getElementById("combNumber")
@@ -43,49 +42,3 @@ submitBtn.addEventListener('click', function(){
     inputEl.value = ''
 })
 
-=======
-const inputEl = document.getElementById("inputValue")
-const secretEl = document.getElementById("readOnly")
-const messageEl = document.getElementById("combNumber")
-
-const startBtn = document.getElementById("start")
-const submitBtn = document.getElementById("submit")
-const pointEl = document.getElementById('count')
-
-let randomNumber = Math.floor(Math.random()*100) + 1
-let gameIsLive = false
-
-startBtn.addEventListener("click", function(){
-    randomNumber = Math.floor(Math.random()*100) + 1
-    gameIsLive = true
-    secretEl.value = randomNumber
-    messageEl.textContent = "Game has started, kindly enter your guess"
-    pointEl.textContent = 0
-})
-
-submitBtn.addEventListener('click', function(){
-    const guess = Number(inputEl.value)
-    const secret = Number(secretEl.value)   
-
-    if (!gameIsLive) {
-           messageEl.textContent = "Please start the game first!"
-           return
-    }else if(inputEl.value === ''){
-        messageEl.textContent = "Kindly enter a valid input"
-
-    }else if(guess === secret){
-        messageEl.textContent = "Congrats player, you nailed it!!"
-        pointEl.textContent = Number(pointEl.textContent) + 3
-        gameIsLive = false
-    }else{
-        if(guess > secret){
-            messageEl.textContent = "Your guess is too high, try again!"
-        }else{
-            messageEl.textContent = "Your guess is too low, try again!"
-        }
-        pointEl.textContent = Number(pointEl.textContent) + 1
-    }
-    inputEl.value = ''
-})
-
->>>>>>> 559cb4f4cddfb9988f160bd2db16f634aeaa2c4e
